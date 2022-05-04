e43d9d7c58004bd25a2a97d921a8d9e7413c66ae
kendlbat <kendlbat@edu.htl-villach.at>
2022-05-04 23:57:13 +0200

    Add transition to automatic image presentation

--- root/2022-wmc-gallery/automatic.js
+++ root/2022-wmc-gallery/automatic.js
@@ -2,6 +2,8 @@ let images;
 let currentImage = 0;
 let progress = 0;
 let progressbarIndex = 0;
+let pauseProgress = true;
+let fadeImageLoaded = false;
 const progressbarAmount = 2;
 const progressbarMinZindex = 5;
 
@@ -19,11 +21,23 @@ async function toNext() {
 
 async function main() {
     images = await getAvailableImages();
+
+    document.getElementById("mainimg").addEventListener("load", () => {
+        pauseProgress = false;
+        document.getElementById("loadingbar").style.visibility = "hidden";
+        document.getElementById("mainimg").classList.add("loaded");
+        document.getElementById("bgimg").classList.add("loaded");
+    });
     
-    document.getElementById("mainimg").src = `images/highres/${Object.values(images)[currentImage]}`;
+    document.getElementById("mainimg").classList.remove("loaded");
+    document.getElementById("bgimg").classList.remove("loaded");
+    document.getElementById("fadebgimg").style.visibility = "hidden";
     document.getElementById("bgimg").src = `images/highres/${Object.values(images)[currentImage]}`;
+    document.getElementById("mainimg").src = `images/highres/${Object.values(images)[currentImage]}`;
+    
     
     setInterval(() => {
+        if (pauseProgress) return;
         progress += 0.2;
 
         if (progress > 100) {
@@ -31,7 +45,18 @@ async function main() {
             document.getElementById("progressbar" + progressbarIndex).style.zIndex = progressbarMinZindex;
             progressbarIndex = (progressbarIndex + 1) % progressbarAmount;
             document.getElementById("progressbar" + progressbarIndex).style.zIndex = progressbarMinZindex + 1;
+            pauseProgress = true;
+            document.getElementById("loadingbar").style.visibility = "visible";
             progress = 0;
+            fadeImageLoaded = false;
+        } else if (!fadeImageLoaded && progress > 20) {
+            fadeImageLoaded = true;
+            document.getElementById("fademainimg").src = document.getElementById("mainimg").src;
+            document.getElementById("fadebgimg").src = document.getElementById("bgimg").src;
+        } else if (progress > 80) {
+            document.getElementById("mainimg").classList.remove("loaded");
+            document.getElementById("bgimg").classList.remove("loaded");
+            document.getElementById("fadebgimg").style.visibility = "visible";
         }
 
         document.getElementById("progressbar" + progressbarIndex).style.left = (progress - 100) + "%";
